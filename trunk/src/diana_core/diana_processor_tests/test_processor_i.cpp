483cdd3988388fb9298c237172b24e76dc0d9a7f
ligen-ua <ligen-ua@2ae6c1b3-f413-431b-9f5c-a2ad07604292>
2011-09-26 09:54:12 +0000

    added test

--- trunk/src/diana_core/diana_processor_tests/test_processor_i.cpp
+++ trunk/src/diana_core/diana_processor_tests/test_processor_i.cpp
@@ -165,8 +165,24 @@ static void test_processor_imul9()
 
     int res = proc.ExecOnce();
     TEST_ASSERT(res == DI_SUCCESS);
-    
+        
     TEST_ASSERT(GET_REG_RAX == 65534);
+    TEST_ASSERT(!GET_FLAG_CF);
+    TEST_ASSERT(!GET_FLAG_OF);
+}
+
+static void test_processor_imul10()
+{
+    unsigned char code[] = {0x66, 0x6B, 0xC2, 0xD3}; // imul        ax,dx,0FFD3h
+    CTestProcessor proc(code, sizeof(code));
+    DianaProcessor * pCallContext = proc.GetSelf();
+
+    SET_REG_EDX(0x17);
+
+    int res = proc.ExecOnce();
+    TEST_ASSERT(res == DI_SUCCESS);
+        
+    TEST_ASSERT(GET_REG_RAX == 0xfbf5);
     TEST_ASSERT(!GET_FLAG_CF);
     TEST_ASSERT(!GET_FLAG_OF);
 }
@@ -197,5 +213,6 @@ void test_processor_i()
     test_processor_imul7();
     test_processor_imul8();
     test_processor_imul9();
+    test_processor_imul10();
     test_processor_inc();
 }
